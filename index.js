fa3a07b8bb7f46a5c9e07f730c232c208b7b53a6
karin yahud <karin167bz@gmail.com>
2021-03-31 16:12:53 +0200

    karin-task

---
+++ index.js
@@ -0,0 +1,67 @@
+//1
+console.log("Exercise 1");
+const park =  "I can walk in the park all day!";
+console.log (park);
+
+//2
+console.log("Exercise 2");
+const upper = "hello world";
+console.log(upper.toUpperCase());
+
+//3
+console.log("Exercise 3");
+const lower = "HELLO EARTHLING"
+console.log(lower.toLowerCase());
+
+//4
+console.log("Exercise 4");
+const js = "JavaScript";
+console.log(js.slice(3 , 6));
+
+//5
+console.log("Exercise 5");
+const centenceOne =  "nice shoes";
+console.log( centenceOne.includes("i" , "e"));
+
+//6
+console.log("Exercise 6");
+const foo = "Banana";
+console.log(foo.slice(0 , 1) + foo + foo[0]);
+
+//7
+console.log("Exercise 7");
+const newString = "Scritch";
+console.log(newString.slice(0 , 3) + newString);
+
+//8
+console.log("Exercise 8");
+const fooTwo = "BoogieWoogie";
+const fooTree = fooTwo.slice(1 ,10);
+console.log(fooTree);
+console.log(fooTwo.slice(11 , 12) + fooTree + fooTwo[0]);
+
+//9
+console.log("Exercise 9");
+let firstName = "Maria";
+let city = "Berlin";
+let profession = "teacher";
+console.log("My name is " + firstName + " i live in " + city + " and i am a " + profession + ".");
+
+//10
+console.log("Exercise 10");
+const vari =  "the quick brown fox";
+const vari2 = vari.slice(0 , 1);
+const vari3 = vari.slice(1 , 20);
+console.log(vari2.toUpperCase() + vari3);
+
+
+
+
+
+
+
+
+
+
+
+
